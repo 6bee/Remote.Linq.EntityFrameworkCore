0ff742873f8bbaf892164f85a076d3af25b76259
Christof Senn <christof.senn@gmail.com>
2017-09-06 06:45:28 +0200

    generating assembly info from project file

--- src/Remote.Linq.EntityFrameworkCore/Properties/AssemblyInfo.cs
+++ src/Remote.Linq.EntityFrameworkCore/Properties/AssemblyInfo.cs
@@ -5,12 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Remote.Linq.EntityFrameworkCore")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Remote.Linq.EntityFrameworkCore")]
-[assembly: AssemblyCompany("Christof Senn")]
-[assembly: AssemblyCopyright("Copyright © Christof Senn 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,20 +14,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ED77C04C-A8D3-4509-8B50-A218C0D2112B")]
-
 [assembly: InternalsVisibleTo("Remote.Linq.EntityFrameworkCore.Test")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Patch Number
-//      Unused
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
