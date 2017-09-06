1989dfe73654b0117a29aa9a01cd76f6875159ea
Christof Senn <christof.senn@gmail.com>
2017-09-06 06:52:08 +0200

    put version-suffix in quotes if empty

--- pack.bat
+++ pack.bat
@@ -6,4 +6,4 @@ clean ^
   && dotnet build src\Remote.Linq.EntityFrameworkCore --configuration %configuration% ^
   && dotnet build test\Remote.Linq.EntityFrameworkCore.Tests --configuration %configuration% ^
   && dotnet test test\Remote.Linq.EntityFrameworkCore.Tests\Remote.Linq.EntityFrameworkCore.Tests.csproj --configuration %configuration% ^
-  && dotnet pack src\Remote.Linq.EntityFrameworkCore\Remote.Linq.EntityFrameworkCore.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix%
+  && dotnet pack src\Remote.Linq.EntityFrameworkCore\Remote.Linq.EntityFrameworkCore.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%"
