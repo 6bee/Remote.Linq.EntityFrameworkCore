04311b021a9e6953369089e7de3c73882616d0eb
Christof Senn <christof.senn@gmail.com>
2017-09-21 07:17:31 +0200

    completed IQueryable factory methods for EF Core

--- src/Remote.Linq.EntityFrameworkCore/RemoteQueryableFactoryExtensions.cs
+++ src/Remote.Linq.EntityFrameworkCore/RemoteQueryableFactoryExtensions.cs
@@ -8,26 +8,45 @@ namespace Remote.Linq.EntityFrameworkCore
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.Linq;
+    using System.Threading.Tasks;
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static class RemoteQueryableFactoryExtensions
     {
         public static IQueryable CreateEntityFrameworkCoreQueryable(this RemoteQueryableFactory factory, Type elementType, Func<Expressions.Expression, IEnumerable<DynamicObject>> dataProvider, ITypeResolver typeResolver = null, IDynamicObjectMapper mapper = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
-            => factory.Create(elementType, dataProvider, typeResolver, mapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+            => factory.CreateQueryable(elementType, dataProvider, typeResolver, mapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
 
         public static IQueryable<T> CreateEntityFrameworkCoreQueryable<T>(this RemoteQueryableFactory factory, Func<Expressions.Expression, IEnumerable<DynamicObject>> dataProvider, ITypeResolver typeResolver = null, IDynamicObjectMapper mapper = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
-            => factory.Create<T>(dataProvider, typeResolver, mapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+            => factory.CreateQueryable<T>(dataProvider, typeResolver, mapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
 
         public static IQueryable CreateEntityFrameworkCoreQueryable(this RemoteQueryableFactory factory, Type elementType, Func<Expressions.Expression, object> dataProvider, ITypeResolver typeResolver = null, IQueryResultMapper<object> resultMapper = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
-            => factory.Create(elementType, dataProvider, typeResolver, resultMapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+            => factory.CreateQueryable(elementType, dataProvider, typeResolver, resultMapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
 
         public static IQueryable<T> CreateEntityFrameworkCoreQueryable<T>(this RemoteQueryableFactory factory, Func<Expressions.Expression, object> dataProvider, ITypeResolver typeResolver = null, IQueryResultMapper<object> resultMapper = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
-            => factory.Create<T>(dataProvider, typeResolver, resultMapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+            => factory.CreateQueryable<T>(dataProvider, typeResolver, resultMapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
 
         public static IQueryable CreateEntityFrameworkCoreQueryable<TSource>(this RemoteQueryableFactory factory, Type elementType, Func<Expressions.Expression, TSource> dataProvider, IQueryResultMapper<TSource> resultMapper, ITypeResolver typeResolver = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
-            => factory.Create<TSource>(elementType, dataProvider, resultMapper, typeResolver, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+            => factory.CreateQueryable<TSource>(elementType, dataProvider, resultMapper, typeResolver, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
 
         public static IQueryable<T> CreateEntityFrameworkCoreQueryable<T, TSource>(this RemoteQueryableFactory factory, Func<Expressions.Expression, TSource> dataProvider, IQueryResultMapper<TSource> resultMapper, ITypeResolver typeResolver = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
-            => factory.Create<T, TSource>(dataProvider, resultMapper, typeResolver, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+            => factory.CreateQueryable<T, TSource>(dataProvider, resultMapper, typeResolver, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+
+        public static IQueryable CreateEntityFrameworkCoreAsyncQueryable(this RemoteQueryableFactory factory, Type elementType, Func<Expressions.Expression, Task<IEnumerable<DynamicObject>>> dataProvider, ITypeResolver typeResolver = null, IDynamicObjectMapper mapper = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
+            => factory.CreateAsyncQueryable(elementType, dataProvider, typeResolver, mapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+
+        public static IQueryable<T> CreateEntityFrameworkCoreAsyncQueryable<T>(this RemoteQueryableFactory factory, Func<Expressions.Expression, Task<IEnumerable<DynamicObject>>> dataProvider, ITypeResolver typeResolver = null, IDynamicObjectMapper mapper = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
+            => factory.CreateAsyncQueryable<T>(dataProvider, typeResolver, mapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+
+        public static IQueryable CreateEntityFrameworkCoreAsyncQueryable(this RemoteQueryableFactory factory, Type elementType, Func<Expressions.Expression, Task<object>> dataProvider, ITypeResolver typeResolver = null, IAsyncQueryResultMapper<object> resultMapper = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
+            => factory.CreateAsyncQueryable(elementType, dataProvider, typeResolver, resultMapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+
+        public static IQueryable<T> CreateEntityFrameworkCoreAsyncQueryable<T>(this RemoteQueryableFactory factory, Func<Expressions.Expression, Task<object>> dataProvider, ITypeResolver typeResolver = null, IAsyncQueryResultMapper<object> resultMapper = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
+            => factory.CreateAsyncQueryable<T>(dataProvider, typeResolver, resultMapper, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+
+        public static IQueryable CreateEntityFrameworkCoreAsyncQueryable<TSource>(this RemoteQueryableFactory factory, Type elementType, Func<Expressions.Expression, Task<TSource>> dataProvider, IAsyncQueryResultMapper<TSource> resultMapper, ITypeResolver typeResolver = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
+            => factory.CreateAsyncQueryable<TSource>(elementType, dataProvider, resultMapper, typeResolver, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
+
+        public static IQueryable<T> CreateEntityFrameworkCoreAsyncQueryable<T, TSource>(this RemoteQueryableFactory factory, Func<Expressions.Expression, Task<TSource>> dataProvider, IAsyncQueryResultMapper<TSource> resultMapper, ITypeResolver typeResolver = null, Func<System.Linq.Expressions.Expression, bool> canBeEvaluatedLocally = null)
+            => factory.CreateAsyncQueryable<T, TSource>(dataProvider, resultMapper, typeResolver, canBeEvaluatedLocally.And(ExpressionEvaluator.CanBeEvaluated));
     }
 }
