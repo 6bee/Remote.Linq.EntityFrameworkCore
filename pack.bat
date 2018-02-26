5ea6c3803bf6aebe54662a5b429b40ba5e92fd87
6bee <christof.senn@gmail.com>
2018-02-27 00:57:51 +0100

    This git repo was integrated in 6bee/Remote.Linq

--- pack.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set configuration=Release
-set version-suffix=""
-clean ^
-  && dotnet restore ^
-  && dotnet build src\Remote.Linq.EntityFrameworkCore --configuration %configuration% ^
-  && dotnet build test\Remote.Linq.EntityFrameworkCore.Tests --configuration %configuration% ^
-  && dotnet test test\Remote.Linq.EntityFrameworkCore.Tests\Remote.Linq.EntityFrameworkCore.Tests.csproj --configuration %configuration% ^
-  && dotnet pack src\Remote.Linq.EntityFrameworkCore\Remote.Linq.EntityFrameworkCore.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%"
